41085436b2a350d5a8472940865bd41dcba8e762
Alexey Mur <aleksmurmur@yahoo.com>
2023-08-28 23:42:50 +0300

    init commit based on BootJava

---
+++ README.md
@@ -0,0 +1,12 @@
+# [Выпускной проект TopJava (на базе BootJava)](https://github.com/JavaWebinar/topjava/blob/doc/doc/graduation.md)
+-------------------------------------------------------------
+- Stack: [JDK 17](http://jdk.java.net/17/), Spring Boot 3.x, Lombok, H2, Caffeine Cache, SpringDoc OpenApi 2.x
+- Run: `mvn spring-boot:run` in root directory.
+-----------------------------------------------------
+[REST API documentation](http://localhost:8080/)
+Креденшелы:
+```
+User:  user@yandex.ru / password
+Admin: admin@gmail.com / admin
+Guest: guest@gmail.com / guest
+```
---
+++ src/main/java/ru/javaops/bootjava/repository/BaseRepository.java
@@ -0,0 +1,38 @@
+package ru.javaops.bootjava.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javaops.bootjava.error.NotFoundException;
+
+import static ru.javaops.bootjava.util.validation.ValidationUtil.checkExisted;
+import static ru.javaops.bootjava.util.validation.ValidationUtil.checkModification;
+
+// https://stackoverflow.com/questions/42781264/multiple-base-repositories-in-spring-data-jpa
+@NoRepositoryBean
+public interface BaseRepository<T> extends JpaRepository<T, Integer> {
+
+    //    https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query.spel-expressions
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM #{#entityName} e WHERE e.id=:id")
+    int delete(int id);
+
+    //  https://stackoverflow.com/a/60695301/548473 (existed delete code 204, not existed: 404)
+    @SuppressWarnings("all") // transaction invoked
+    default void deleteExisted(int id) {
+        if (delete(id) == 0) {
+            throw new NotFoundException("Entity with id=" + id + " not found");
+        }
+    }
+
+
+    @Query("SELECT e FROM #{#entityName} e WHERE e.id = :id")
+    T get(int id);
+
+    default T getExisted(int id) {
+        return checkExisted(get(id), id);
+    }
+}
